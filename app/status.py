2931854b26b2737b92508dd5f4888d67cd2308d6
Srijay Kasturi <srijay@techfilmer.com>
2014-04-14 18:44:41 +0000

    +load avg

--- app/status.py
+++ app/status.py
@@ -5,5 +5,5 @@ from app import values
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", server = 'rio', uptime = values.uptime(), user = 'skasturi', load = '0.12, 0.10, 0.14', memory = '1 GB')
+    return render_template("index.html", server = 'rio', uptime = values.uptime(), user = 'skasturi', load = values.loadaverage(), memory = '1 GB')
 
--- app/values.py
+++ app/values.py
@@ -1,7 +1,17 @@
 import subprocess
-cmd = subprocess.check_output('uptime', stderr=subprocess.STDOUT).decode().strip().replace('\n', '')
+import psutil
 
 def uptime():
+    cmd = subprocess.check_output('uptime', stderr=subprocess.STDOUT).decode().strip().replace('\n', '')
     uptime = "".join("".join(cmd.split(',', maxsplit=3)[:2]).split("up")[1:]).replace(" days", "").split()
     uptim = uptime[1].split(":")
     return "%s days, %s hours, %s minutes" % (uptime[0], uptim[0], uptim[1])
+
+def loadaverage():
+    cmd = subprocess.check_output('uptime', stderr=subprocess.STDOUT).decode().strip().replace('\n', '')
+    loadaverage = cmd.split(',', maxsplit=3)[3].strip().replace(",", "")
+    return loadaverage
+
+def memory():
+    #svmem(total=620265472, available=190590976, percent=69.3, used=531537920, free=88727552, active=421326848, inactive=57815040, buffers=19206144, cached=82657280)
+    mem = psutil.phymem_usage()
