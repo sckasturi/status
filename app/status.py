41396b43638d99e62f5554df0fd1f552e31babd2
Srijay Kasturi <srijay@techfilmer.com>
2014-04-14 00:34:53 -0400

    Adding more stuff :D

--- app/status.py
+++ app/status.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html", server = 'rio', uptime = '42 days', user = 'skasturi', load = '0.12, 0.10, 0.14', memory = '1 GB')
+
