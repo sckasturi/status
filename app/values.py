a6640d9039288662a678335fe4df5475ddc834ec
Srijay Kasturi <srijay@techfilmer.com>
2014-04-15 16:01:13 +0000

    And that's most of the values :D

--- app/values.py
+++ app/values.py
@@ -8,11 +8,18 @@ def uptime():
     return "%s days, %s hours, %s minutes" % (uptime[0], uptim[0], uptim[1])
 
 def loadaverage():
-    cmd = subprocess.check_output('uptime', stderr=subprocess.STDOUT).decode().strip().replace('\n', '')
-    loadaverage = cmd.split(',', maxsplit=3)[3].strip().replace(",", "")
-    return loadaverage
+    cmd = subprocess.check_output(['cat', '/proc/loadavg'], stderr=subprocess.STDOUT).decode().strip().replace('\n', '')
+    #loadaverage = cmd.split(',', maxsplit=3)[3].strip().replace(",", "")
+    return cmd
 
 def memory():
     #svmem(total=620265472, available=190590976, percent=69.3, used=531537920, free=88727552, active=421326848, inactive=57815040, buffers=19206144, cached=82657280)
     mem = psutil.phymem_usage()
     return ["%s%s memory avaliable." % (mem[2], "%"), "Total: %s, Avaliable: %s, Used %s" % (mem[0], mem[1], mem[4])]
+
+def users():
+    cmd = subprocess.check_output(['who', '-q'], stderr=subprocess.STDOUT).decode().strip().split("\n")
+    return cmd[0]
+
+def hostname():
+    return subprocess.check_output('hostname', stderr=subprocess.STDOUT).decode().strip().replace("\n", "")
